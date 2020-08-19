65346bf1084e9804bb2400e72095bff9bb5ccf0a
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2020-08-19 12:33:48 -0500

    otf added to build

--- src/build.sh
+++ src/build.sh
@@ -16,4 +16,15 @@ do
     mv "$ttf.fix" $ttf
 done
 
+echo = "Generating OTFs"
+fontmake -m ./UFO/Castoro_Roman.designspace -o otf --output-dir ../fonts/otf -a
+fontmake -m ./UFO/Castoro_Italic.designspace -o otf --output-dir ../fonts/otf -a
+
+echo "Post processing static OTFs"
+otf=$(ls ../fonts/otf/*.otf)
+for otf in $otf
+do
+	gftools fix-dsig -f $otf
+done
+
 echo "Done!"
